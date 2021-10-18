fae90c8f21db835ed9f6c85387e011489fcc972f
PJ Manning <phil.manning11@gmail.com>
2021-10-18 23:27:21 +0800

    set up tailwind config

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  mode: 'jit',
+  purge: [
+    './components/**/*.{vue,js}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}',
+  ],
+  darkMode: 'class', // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['"Inter var"', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
