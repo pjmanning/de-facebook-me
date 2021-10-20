9f37bd57be9724a3f6ab222462d188d01bd52364
PJ Manning <phil.manning11@gmail.com>
2021-10-20 16:26:23 +0800

    adding sitemap and plausible

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,12 +4,83 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s | De-Facebook-Me',
+    titleTemplate: '%s | DeFacebookMe',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
       { name: 'format-detection', content: 'telephone=no' },
+      {
+        hid: 'description',
+        name: 'description',
+        content:
+          'Remove your Facebook Footprint. How to best Delete/Deactivate/Take a break and how to save your data and contacts.',
+      },
+      //   // twitter-card: https://cards-dev.twitter.com/validator
+      //   { name: 'twitter:card', content: 'summary_large_image' },
+      //   { name: 'twitter:site', content: '@GlassyHQ' },
+      //   { name: 'twitter:creator', content: '@pj_manning' },
+      //   {
+      //     hid: 'twitter:title',
+      //     name: 'twitter:title',
+      //     content: 'Glassy | Make Your Company More Transparent',
+      //   },
+      //   {
+      //     hid: 'twitter:description',
+      //     name: 'twitter:description',
+      //     content:
+      //       '🔎  Create an open company culture by being transparent with your investors, employees, customers, and other stakeholders.',
+      //   },
+      //   {
+      //     hid: 'twitter:image',
+      //     name: 'twitter:image',
+      //     content:
+      //       'https://firebasestorage.googleapis.com/v0/b/glassy-so.appspot.com/o/glassy-ogimage.png?alt=media&token=359850a5-cb80-4b02-8777-c611d53a2769',
+      //   },
+      //   {
+      //     hid: 'twitter:image:alt',
+      //     name: 'twitter:image:alt',
+      //     content: 'Glassy',
+      //   },
+
+      //   // facebook card https://developers.facebook.com/tools/debug/
+      //   {
+      //     hid: 'fb:app_id',
+      //     property: 'fb:app_id',
+      //     content: '1013298389488671',
+      //   },
+      //   {
+      //     hid: 'og:url',
+      //     property: 'og:url',
+      //     content: 'https://glassy.so',
+      //   },
+      //   { hid: 'og:type', property: 'og:type', content: 'website' },
+      //   {
+      //     hid: 'og:title',
+      //     property: 'og:title',
+      //     content: 'Glassy | Make Your Company More Transparent',
+      //   },
+      //   {
+      //     hid: 'og:image',
+      //     property: 'og:image',
+      //     content:
+      //       'https://firebasestorage.googleapis.com/v0/b/glassy-so.appspot.com/o/glassy-ogimage.png?alt=media&token=359850a5-cb80-4b02-8777-c611d53a2769',
+      //   },
+      //   {
+      //     hid: 'og:description',
+      //     property: 'og:description',
+      //     content:
+      //       '🔎  Create an open company culture by being transparent with your investors, employees, customers, and other stakeholders.',
+      //   },
+      //   {
+      //     hid: 'og:site_name',
+      //     property: 'og:site_name',
+      //     content: 'Glassy',
+      //   },
+      //   {
+      //     hid: 'og:image:alt',
+      //     property: 'og:image:alt',
+      //     content: 'Glassy',
+      //   },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
   },
@@ -41,7 +112,17 @@ export default {
     '@nuxtjs/pwa',
     // https://go.nuxtjs.dev/content
     '@nuxt/content',
+    '@nuxtjs/sitemap',
+    'vue-plausible',
   ],
+
+  sitemap: {
+    hostname: process.env.BASE_URL || 'https://defacebookme.com',
+  },
+
+  plausible: {
+    domain: 'defacebookme.com',
+  },
 
   // PWA module configuration: https://go.nuxtjs.dev/pwa
   pwa: {
