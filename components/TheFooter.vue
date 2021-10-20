0fb6e03553a00bbe78d55cf8a5bac8a8bf51afdf
PJ Manning <phil.manning11@gmail.com>
2021-10-20 16:59:57 +0800

    footer base

--- components/TheFooter.vue
+++ components/TheFooter.vue
@@ -1,64 +1,85 @@
 <template>
-  <footer class="bg-white">
-    <div class="max-w-7xl mx-auto py-12 px-4 overflow-hidden sm:px-6 lg:px-8">
-      <nav class="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
-        <div class="px-5 py-2">
-          <a href="#" class="text-base text-gray-500 hover:text-gray-900"> About </a>
-        </div>
-
-        <div class="px-5 py-2">
-          <a href="#" class="text-base text-gray-500 hover:text-gray-900"> Blog </a>
-        </div>
-
-        <div class="px-5 py-2">
-          <a href="#" class="text-base text-gray-500 hover:text-gray-900"> Jobs </a>
-        </div>
-
-        <div class="px-5 py-2">
-          <a href="#" class="text-base text-gray-500 hover:text-gray-900"> Press </a>
-        </div>
-
-        <div class="px-5 py-2">
-          <a href="#" class="text-base text-gray-500 hover:text-gray-900"> Accessibility </a>
-        </div>
-
-        <div class="px-5 py-2">
-          <a href="#" class="text-base text-gray-500 hover:text-gray-900"> Partners </a>
-        </div>
-      </nav>
-      <div class="mt-8 flex justify-center space-x-6">
-        <a href="#" class="text-gray-400 hover:text-gray-500">
-          <span class="sr-only">Twitter</span>
-          <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-            <path
-              d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84"
-            />
-          </svg>
-        </a>
-
-        <a href="#" class="text-gray-400 hover:text-gray-500">
-          <span class="sr-only">GitHub</span>
-          <svg class="h-6 w-6" fill="currentColor" viewBox="0 0 24 24" aria-hidden="true">
-            <path
-              fill-rule="evenodd"
-              d="M12 2C6.477 2 2 6.484 2 12.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0112 6.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.202 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.943.359.309.678.92.678 1.855 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0022 12.017C22 6.484 17.522 2 12 2z"
-              clip-rule="evenodd"
-            />
-          </svg>
-        </a>
-      </div>
-      <p class="mt-8 text-center text-base text-gray-400">
-        🇮🇩 2021 Made by
-        <a
-          href="https://pjmanning.dev"
-          target="_blank"
-          class="text-gray-700 hover:text-gray-500 font-medium"
-          >PJManning</a
+  <section class="py-10 bg-black">
+    <div class="px-10 mx-auto max-w-7xl">
+      <div class="flex flex-col items-center md:flex-row md:justify-between">
+        <nuxt-link
+          to="/"
+          class="flex items-center justify-center w-10 h-10 mr-3 rounded-lg mb-7 bg-gradient-to-br from-purple-500 via-indigo-600 to-blue-500 rounded-xxl"
         >
-        in Bali, Indonesia
-      </p>
+          <svg
+            class="w-5 h-5 text-white fill-current"
+            viewBox="0 0 39 15"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M11.252 1.152C4.184 2.526.454 6.918.061 14.329c1.963-4.049 4.798-5.975 8.503-5.778 2.115.112 3.84 1.295 5.75 2.603 3.11 2.133 6.712 4.601 13.281 3.324 7.068-1.374 10.798-5.766 11.191-13.177-1.963 4.049-4.798 5.975-8.503 5.779-2.115-.113-3.84-1.296-5.75-2.604-3.11-2.133-6.712-4.601-13.281-3.324z"
+              fill-rule="evenodd"
+            ></path>
+          </svg>
+        </nuxt-link>
+
+        <div class="flex flex-row justify-center mb-4 -ml-4 -mr-4">
+          <a
+            href="https://twitter.com/pj_manning"
+            target="_blank"
+            class="p-4 text-gray-700 hover:text-gray-400"
+          >
+            <svg class="fill-current" width="16" height="16" xmlns="http://www.w3.org/2000/svg">
+              <path
+                d="M16 3c-.6.3-1.2.4-1.9.5.7-.4 1.2-1 1.4-1.8-.6.4-1.3.6-2.1.8-.6-.6-1.5-1-2.4-1-1.7 0-3.2 1.5-3.2 3.3 0 .3 0 .5.1.7-2.7-.1-5.2-1.4-6.8-3.4-.3.5-.4 1-.4 1.7 0 1.1.6 2.1 1.5 2.7-.5 0-1-.2-1.5-.4C.7 7.7 1.8 9 3.3 9.3c-.3.1-.6.1-.9.1-.2 0-.4 0-.6-.1.4 1.3 1.6 2.3 3.1 2.3-1.1.9-2.5 1.4-4.1 1.4H0c1.5.9 3.2 1.5 5 1.5 6 0 9.3-5 9.3-9.3v-.4C15 4.3 15.6 3.7 16 3z"
+              ></path>
+            </svg>
+          </a>
+          <a
+            href="https://github.com/pjmanning/de-facebook-me"
+            target="_blank"
+            class="p-4 text-gray-700 hover:text-gray-400"
+          >
+            <svg
+              class="fill-current"
+              width="16"
+              height="16"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                d="M12 0c-6.626 0-12 5.373-12 12 0 5.302 3.438 9.8 8.207 11.387.599.111.793-.261.793-.577v-2.234c-3.338.726-4.033-1.416-4.033-1.416-.546-1.387-1.333-1.756-1.333-1.756-1.089-.745.083-.729.083-.729 1.205.084 1.839 1.237 1.839 1.237 1.07 1.834 2.807 1.304 3.492.997.107-.775.418-1.305.762-1.604-2.665-.305-5.467-1.334-5.467-5.931 0-1.311.469-2.381 1.236-3.221-.124-.303-.535-1.524.117-3.176 0 0 1.008-.322 3.301 1.23.957-.266 1.983-.399 3.003-.404 1.02.005 2.047.138 3.006.404 2.291-1.552 3.297-1.23 3.297-1.23.653 1.653.242 2.874.118 3.176.77.84 1.235 1.911 1.235 3.221 0 4.609-2.807 5.624-5.479 5.921.43.372.823 1.102.823 2.222v3.293c0 .319.192.694.801.576 4.765-1.589 8.199-6.086 8.199-11.386 0-6.627-5.373-12-12-12z"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
+      <div class="flex flex-col justify-between text-center md:flex-row">
+        <p class="order-last text-sm leading-tight text-gray-500 md:order-first">
+          Made by
+          <a
+            href="https://pjmanning.dev"
+            target="_blank"
+            class="font-medium text-white hover:text-gray-200"
+            >PJManning</a
+          >
+          in Bali, Indonesia 🇮🇩
+        </p>
+        <ul class="flex flex-row justify-center pb-3 -ml-4 -mr-4 text-sm">
+          <li>
+            <nuxt-link to="/facebook" class="px-4 text-gray-500 hover:text-white"
+              >Facebook</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/instagram" class="px-4 text-gray-500 hover:text-white"
+              >Instagram</nuxt-link
+            >
+          </li>
+          <li>
+            <nuxt-link to="/whatsapp" class="px-4 text-gray-500 hover:text-white"
+              >WhatsApp</nuxt-link
+            >
+          </li>
+        </ul>
+      </div>
     </div>
-  </footer>
+  </section>
 </template>
 
 <script>
